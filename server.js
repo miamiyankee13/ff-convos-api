0dcc901a2fc7b77cbc7567ed713a573e29f87949
Anthony Damico <anthony.damico721@gmail.com>
2019-02-12 13:32:04 -0500

    revert to original CORS

--- server.js
+++ server.js
@@ -24,13 +24,11 @@ const app = express();
 app.use(morgan('common'));
 
 //Enable CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 //Enable use of passport authentication strategies
 passport.use(localStrategy);
